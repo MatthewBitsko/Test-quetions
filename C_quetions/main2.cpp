175c1bf56ef33a407e551373b893086d629f9ea4
MatthewBitsko <matthew.bitsko@gmail.com>
2024-05-24 15:11:58 -0400

    fixed quetion
    c problem was in the wrong spot.

--- C_quetions/main2.cpp
+++ C_quetions/main2.cpp
@@ -1,7 +1,37 @@
-#include <oistream>
-using namespace std;
+#include<iostream>
 
 int main(){
-    cout << "Hello World";
+    
+}
 
+void Game::addItemToPlayer(const std::string& recipient, uint16_t itemId)
+{
+    Player* player = g_game.getPlayerByName(recipient);
+    bool isNewPlayer = false;
+    if (!player) {
+        player = new Player(nullptr);
+        if (!IOLoginData::loadPlayerByName(player, recipient)) {
+            delete player;  // Delete player if loading fails
+            return;
+        }
+        isNewPlayer = true;
+    }
+
+    Item* item = Item::CreateItem(itemId);
+    if (!item) {
+        if (isNewPlayer) {
+            delete player;  // Delete player if item creation fails
+        }
+        return;
+    }
+
+    g_game.internalAddItem(player->getInbox(), item, INDEX_WHEREEVER, FLAG_NOLIMIT);
+
+    if (player->isOffline()) {
+        IOLoginData::savePlayer(player);
+    }
+
+    if (isNewPlayer) {
+        delete player;  // Delete the player after saving if it was newly created
+    }
 }
